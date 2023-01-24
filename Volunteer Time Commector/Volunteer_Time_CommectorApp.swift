01262f49f37e4f06e97697d0b8b9bdd88ab46b1d
christopherlayton <christopherlayton@gmail.com>
2023-01-23 18:32:17 -0800

    Initial Commit

---
+++ Volunteer Time Commector/Volunteer_Time_CommectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Volunteer_Time_CommectorApp.swift
+//  Volunteer Time Commector
+//
+//  Created by Christopher Layton on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Volunteer_Time_CommectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
